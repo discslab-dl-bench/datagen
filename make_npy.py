0572cc6500fbcceb2131bbbe3e503cddec468b29
Loic Ho-Von <loic.hovon@gmail.com>
2022-05-19 17:11:51 -0400

    added ability to reduce dataset size

--- make_npy.py
+++ make_npy.py
@@ -1,11 +1,16 @@
 import numpy as np
+import pathlib
 
 if __name__ == "__main__":
 
-    for i in range(300):
+    pathlib.Path("./data").mkdir(parents=True, exist_ok=True)
+
+    for i in range(210):
         casename = "case_" + f"{i:05}"  # Pad case number with zeros to be 5 digits long
 
         dim = np.random.randint(35, 75)
         m = np.random.rand(dim, dim, dim)
-        with open(f"./data/{casename}.npy", "wb") as f:
-            np.save(f, m)
+
+        for xy in ["x", "y"]:
+            with open(f"./data/{casename}_{xy}.npy", "wb") as f:
+                np.save(f, m)
